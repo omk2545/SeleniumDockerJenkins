296ac0455a7d1c32f33aa7dbfd373eb197630c6e
omk2545 <omk2545@gmail.com>
2020-10-30 16:43:02 +0530

    Jenkins working fix

--- testrun.sh
+++ testrun.sh
@@ -3,9 +3,11 @@
 # HUB_HOST
 # BROWSER
 # MODULE
-if [ $1 == null ]; then
-$1= 'search-module'
+if [ "$1" == null ]; then
+"$1"= 'search-module'
 fi
 
+
+source ~./bash_profile
 echo " ********* Running the  $1 suite************** "
 mvn clean install test -DsuiteXmlFile=$1.xml
