37fb1ad9891d108eb4c6339119160f8128e311ff
omk2545 <omk2545@gmail.com>
2020-10-30 16:31:13 +0530

    Added new sh file for execution

---
+++ testrun.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Environment Variables
+# HUB_HOST
+# BROWSER
+# MODULE
+if [ $1 == null ]; then
+$1= 'search-module'
+fi
+
+echo " ********* Running the  $1 suite************** "
+mvn clean install test -DsuiteXmlFile=$1.xml
